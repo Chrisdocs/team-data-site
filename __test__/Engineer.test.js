5d4448cff2760ecc5ad68fe65a94d112c068cba5
Chris Wolfe <chrisa.wolfe@gmail.com>
2021-05-21 18:53:51 -0500

    user input is pushed to contructors

--- __test__/Engineer.test.js
+++ __test__/Engineer.test.js
@@ -1,7 +1,7 @@
 const Engineer = require('../lib/Engineer.js');
 
 test('Gets an engineers github username', () => {
-    const engineer = new Engineer('Username');
+    const engineer = new Engineer('jon', '53683', 'email@email', 'Username');
 
     expect(engineer.github).toEqual(expect.any(String));
     expect(engineer.getGithub()).toBe('github.com/Username');
--- __test__/Manager.test.js
+++ __test__/Manager.test.js
@@ -1,7 +1,7 @@
 const Manager = require('../lib/Manager.js');
 
 test('Gets manager office number', () => {
-    const manager = new Manager('123');
+    const manager = new Manager('Sue', '12435', 'email',123);
 
     expect(manager.officeNum).toEqual(expect.any(Number));
     expect(manager.getRole()).toEqual(expect.any(String));
